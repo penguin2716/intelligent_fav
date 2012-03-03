e7fc151482f9da25feee369306100bafc06b54c4
Takuma Nakajima <penguin.trance.2716@gmail.com>
2012-03-04 05:01:10 +0900

    fix bug

--- intelligent_fav.rb
+++ intelligent_fav.rb
@@ -3,19 +3,19 @@
 Plugin.create :intelligent_fav do
   filter_command() { |menu|
     menu[:intelligent_fav] = {
-      :name => 'ふぁぼふぁぼ！',
+      :slug => :intelligent_fav,
+      :name => 'ふぁぼふぁぼ！！！',
       :condition => ret_nth,
       :exec => lambda{ |ms|
-        Thread.new{
         ms.each { |m|
-            if !m.system? and m.favoritable?
-              m.favorite(true)
-            end
-          }
+          if !m.message.system? and m.message.favoritable?
+            m.message.favorite(true)
+          end
         }
       },
       :visible => true,
       :role => :messages
     }
+    [menu]
   }
 end
