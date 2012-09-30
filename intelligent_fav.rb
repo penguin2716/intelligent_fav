fc98ff1c1c767b3a35b697aae170f950128923d1
Takuma Nakajima <penguin.trance.2716@gmail.com>
2012-09-30 20:25:02 +0900

    compatible with mikutter 0.2

--- intelligent_fav.rb
+++ intelligent_fav.rb
@@ -1,21 +1,17 @@
 #-*- coding: utf-8 -*-
 
 Plugin.create :intelligent_fav do
-  filter_command() { |menu|
-    menu[:intelligent_fav] = {
-      :slug => :intelligent_fav,
-      :name => 'ふぁぼふぁぼ！！！',
-      :condition => ret_nth,
-      :exec => lambda{ |ms|
-        ms.each { |m|
-          if !m.message.system? and m.message.favoritable?
-            m.message.favorite(true)
-          end
-        }
-      },
-      :visible => true,
-      :role => :messages
+
+  command(:intelligent_fav,
+          name: 'ふぁぼふぁぼ！！！',
+          condition: Plugin::Command[:HasMessage],
+          visible: true,
+          role: :timeline) do |opt|
+    opt.messages.each { |m|
+      if !m.message.system? and m.message.favoritable?
+        m.message.favorite(true)
+      end
     }
-    [menu]
-  }
+  end 
+
 end
